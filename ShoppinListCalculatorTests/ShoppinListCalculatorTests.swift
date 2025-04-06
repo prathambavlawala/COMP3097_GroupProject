c458c71ebb13b87b163ce87e53352da3e36447e6
Pratham <bavlawalapratham@gmail.com>
2025-04-06 18:40:17 -0400

    first commit

---
+++ ShoppinListCalculatorTests/ShoppinListCalculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppinListCalculatorTests.swift
+//  ShoppinListCalculatorTests
+//
+//  Created by Jayesh Patel on 2025-03-02.
+//
+
+import Testing
+@testable import ShoppinListCalculator
+
+struct ShoppinListCalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
